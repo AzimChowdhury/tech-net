f4742428ded8de23dd2722b6d9833c02dd1ea835
Azim Chowdhury <azimchy994@gmail.com>
2023-07-12 19:47:18 +0800

    auth with redux

--- src/redux/features/user/userSlice.ts
+++ src/redux/features/user/userSlice.ts
@@ -35,9 +35,21 @@ const userSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(createUser.pending, (state) => {
-      state.isLoading = true;
-    });
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.error = null;
+      })
+      .addCase(createUser.fulfilled, (state, action) => {
+        state.user.email = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.user.email = null;
+        state.isError = true;
+        state.error = action.error.message!;
+      });
   },
 });
 
